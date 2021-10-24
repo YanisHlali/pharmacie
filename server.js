6097f5f91a61ee711be833a130523e86963def00
YanisHlali <78662780+YanisHlali@users.noreply.github.com>
2021-10-24 20:07:14 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -21,25 +21,68 @@ db.connect(function(err) {
     console.log("Connecté à la base de données MySQL!")
 });
 
+function dateUS_toFR(date) {
+    date = date.toString().split(" ")
+    return date[2] + " " + date[1] + " " + date[3]
+}
+function formatTelephone(num) {
+    num = num.toString().split("")
+    for (let i = 1; i < num.length; i = i + 2) {
+        num[i] = " "+num[i]
+    }
+    num.unshift(0)
+    return num.join("")
+}
 
 app.get("/", (req,res) => {
     res.render("index.ejs")
 })
 
-app.get("/views/recherche.ejs",(req,res) => {
-    res.render("recherche.ejs")
+app.get("/views/index.ejs",(req,res) => {
+    res.render("index.ejs")
 })
 
-app.post("/views/recherche.ejs",(req,res) => {
-    db.query("SELECT * FROM patients WHERE nom='"+req.body.nom+"'",(err,result) => {
-        if (err) throw err;
+app.get("/views/liste.ejs",(req,res) => {
+    db.query("SELECT * FROM patients",(err,result) => {
+        res.render("liste.ejs", { result: result })
+    })
+})
+
+app.post("/views/liste.ejs",(req,res) => {
+    if (req.body.nom == "") {
+        db.query("SELECT * FROM patients",(err,result) => {
+            for (let i = 0; i < result.length; i++) {result[i].telephone = formatTelephone(result[i].telephone)}
+            res.render("liste.ejs", { result: result })
+        })
+    } else {
+        db.query(`SELECT * FROM patients WHERE nom LIKE '%${req.body.nom}%' OR prenom LIKE '%${req.body.nom}%'`,(err,result) => {
+            if (err) throw err;
+            if (result != "") {
+                result[0].telephone = formatTelephone(result[0].telephone)
+                res.render("liste.ejs", { result: result })
+            } else {
+                res.redirect("/views/index.ejs")
+            }
+        })
+    }
+})
+
+app.get("/views/informations.ejs/:id",(req,res) => {
+    db.query(`SELECT * FROM patients WHERE id=${req.params.id}`,(err,result) => {
         if (result != "") {
-            console.log(result)
-            res.render("recherche.ejs", { result: result})
+            result[0].DateDeNaissance = dateUS_toFR(result[0].DateDeNaissance)
+            res.render("informations.ejs", { result: result })
+        } else {
+            res.render("informations.ejs", { result: "" })
         }
     })
 })
 
+app.get("/views/stock.ejs",(req,res)=> {
+    db.query("SELECT Nom,Quantité FROM stock",(err,result) => {
+        res.render("stock.ejs",{ result: result })
+    })
+})
 
 app.listen(port, () => {
     console.log('Ecoute du port ' + port)
