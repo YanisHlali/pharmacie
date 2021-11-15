b885786ba7a4a905983040030b621a1a56755925
YanisHlali <yanis.hlali@gmail.com>
2021-11-15 23:50:07 +0100

    TEST

---
+++ routes/routes.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const routeur = express.Router();
+const controle = require('../controller/controller.js')
+
+routeur.get('/',controle.index)
+.get('/views/index.ejs',controle.index)
+.get('/views/liste.ejs',controle.listeGET)
+.post('/views/liste.ejs',controle.listePOST)
+.get('/views/informations.ejs/modifierPatient/:id',controle.modifierPatientGET)
+.post('/views/informations.ejs/modifierPatient/:id',controle.modifierPatientPOST)
+.get('/views/informations.ejs/supprimerPatient/:id',controle.supprimerPatient)
+.get('/views/informations.ejs/:id',controle.informationsGET)
+.post('/views/informations.ejs/:id/ordonnance',controle.informationsPOST)
+.get('/views/informations.ejs/ordonnance/:id',controle.ordonnance)
+.get('/views/informations.ejs/ajoutTraitement/:id',controle.ajoutTraitementGET)
+.post('/views/informations.ejs/ajoutTraitement/:id',controle.ajoutTraitementPOST)
+.get('/views/informations.ejs/modifierTraitement/:id',controle.modifierTraitementGET)
+.post('/views/informations.ejs/modifierTraitement/:id/:idTraitement',controle.modifierTraitementPOST)
+.get('/views/informations.ejs/supprimerTraitement/:id',controle.supprimerTraitement)
+.get('/views/stock.ejs',controle.stock)
+.get('/views/nouveau.ejs',controle.nouveauGET)
+.post('/views/nouveau.ejs',controle.nouveauPOST)
+module.exports = routeur
