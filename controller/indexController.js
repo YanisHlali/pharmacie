a79171e5c043ce1479be28511f344174cc0db5e2
YanisHlali <yanis.hlali@gmail.com>
2021-11-23 18:49:52 +0100

    VERSION FINALE

--- controller/indexController.js
+++
@@ -1,8 +0,0 @@
-// Affiche la page d'accueil
-async function index (req,res) {
-    res.render("index.ejs")
-}
-// Exporte les fonctions
-module.exports = {
-    index
-}
--- routes/routeListe.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const routeur = express.Router();
-const listeControle = require('../controller/listeController.js')
-
-routeur
-.get('/',listeControle.affichageListe)
-.post('/',listeControle.rechercheListe)
-
-module.exports = routeur
